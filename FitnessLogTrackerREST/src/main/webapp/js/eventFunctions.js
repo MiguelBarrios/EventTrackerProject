c68c543872101943aae6314b039f16483074117c
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-05-13 18:23:14 -0500

    created form to create new event

--- FitnessLogTrackerREST/src/main/webapp/js/eventFunctions.js
+++ FitnessLogTrackerREST/src/main/webapp/js/eventFunctions.js
@@ -1,5 +1,7 @@
 window.addEventListener('load', function() {
 	init();
+
+	document.addEventForm.addEventBtn.addEventListener('click',createNewEvent);
 });
 
 function init() {
@@ -63,4 +65,65 @@ function createAndAppendElement(tag, content, parent) {
 	let item = document.createElement(tag);
 	item.textContent = content;
 	parent.appendChild(item);
+}
+
+function createNewEvent(event){
+	event.preventDefault();
+
+	let form = document.addEventForm;
+
+	let name = form.exerciseName.value;
+	let weight = form.weight.value;
+	let reps = form.reps.value;
+	let type = form.type.value;
+	let dateTime = new Date();
+	let dateStr = formatDate(dateTime);
+	let newEvent = {
+    exerciseName : name,
+    weight: weight,
+    reps: reps,
+    type: type,
+    datetime: dateStr
+	}
+
+	let xhr = new XMLHttpRequest();
+	xhr.open('POST', 'api/exerciseset', true);
+
+	xhr.setRequestHeader("Content-type", "application/json"); // Specify JSON request body
+
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status == 200 || xhr.status == 201) { // Ok or Created
+				let film = JSON.parse(xhr.responseText);
+				console.log(film);
+			}
+			else {
+				console.error("POST request failed.");
+				console.error(xhr.status + ': ' + xhr.responseText);
+			}
+		}
+	}
+
+	let eventJson = JSON.stringify(newEvent); // Convert JS object to JSON string
+
+	// Pass JSON as request body
+	xhr.send(eventJson);
+
+
+}
+
+function formatDate(date){
+
+	let year = date.getFullYear();
+	let month = numberStr(date.getMonth());
+	let day = numberStr(date.getDate());
+	let hour = numberStr(date.getHours());
+	let minutes = numberStr(date.getMinutes());
+	let seconds = numberStr(date.getSeconds());
+	let dateStr = year + "-" + month + "-" + day + " " + hour + ":" + minutes + ":" + seconds;
+	console.log(dateStr);
+}
+
+function numberStr(number){
+	return (number <= 9) ?  "0" + number : number;
 }
