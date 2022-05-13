988af97141efd4d925b5e091e69930106ca340e6
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-05-13 17:18:33 -0500

    Completed index event front end

--- DB/fitnesslog.sql
+++ DB/fitnesslog.sql
@@ -16,13 +16,17 @@ CREATE SCHEMA IF NOT EXISTS `fitnesslogdb` DEFAULT CHARACTER SET utf8 ;
 USE `fitnesslogdb` ;
 
 -- -----------------------------------------------------
--- Table `exercise`
+-- Table `exercise_set`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `exercise` ;
+DROP TABLE IF EXISTS `exercise_set` ;
 
-CREATE TABLE IF NOT EXISTS `exercise` (
+CREATE TABLE IF NOT EXISTS `exercise_set` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(500) NOT NULL,
+  `exercise_name` VARCHAR(500) NULL,
+  `weight` DECIMAL(8,2) NULL,
+  `reps` INT NULL,
+  `type` VARCHAR(45) NULL,
+  `datetime` DATETIME NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
@@ -38,11 +42,15 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
--- Data for table `exercise`
+-- Data for table `exercise_set`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `fitnesslogdb`;
-INSERT INTO `exercise` (`id`, `name`) VALUES (1, 'BENCH PRESS');
+INSERT INTO `exercise_set` (`id`, `exercise_name`, `weight`, `reps`, `type`, `datetime`) VALUES (1, 'Bench Press', 135, 15, 'warmup', '2022-02-15T08:30:23');
+INSERT INTO `exercise_set` (`id`, `exercise_name`, `weight`, `reps`, `type`, `datetime`) VALUES (2, 'Bench Press', 245, 4, 'strength', NULL);
+INSERT INTO `exercise_set` (`id`, `exercise_name`, `weight`, `reps`, `type`, `datetime`) VALUES (3, 'Bench Press', 185, 12, 'endurance', NULL);
+INSERT INTO `exercise_set` (`id`, `exercise_name`, `weight`, `reps`, `type`, `datetime`) VALUES (4, 'Squat', 225, 10, 'warmup', NULL);
+INSERT INTO `exercise_set` (`id`, `exercise_name`, `weight`, `reps`, `type`, `datetime`) VALUES (5, 'Squat', 315, 6, 'strength', NULL);
 
 COMMIT;
 
